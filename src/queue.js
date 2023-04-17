3dbfd721a5898616eccfab57db71d8e6eee3f134
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2023-04-17 23:59:02 +0300

    last commit

--- src/queue.js
+++ src/queue.js
@@ -31,12 +31,6 @@ class Queue {
   }
 }
 
- const queue = new Queue();
-
- queue.enqueue(1); // adds the element to the queue
- queue.enqueue(3); // adds the element to the queue
- queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- queue.getUnderlyingList() // returns { value: 3, next: null }
 
 module.exports = {
   Queue
